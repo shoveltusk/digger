bdb71f7154f6b0e38df8b40fdbed5d8139a71c6b
shoveltusk <patrick.r.babcock@gmail.com>
2012-12-16 15:57:42 -0500

    added curl call to get voter

--- fs-test.js
+++ fs-test.js
@@ -1,8 +1,35 @@
-    session = new Session('sofia/external/babcockp@iptel.org');
-    //new comment
-    session.waitForAnswer(10000);
- 
-    if (session.ready()) {
-        session.sleep(1000);
-        session.streamFile('/usr/local/freeswitch/sounds/en/us/demos/fs-test.wav');
-    }
+use("CURL");
+/*
+function my_callback(string, arg)
+{
+  console_log("info", string);
+  return true;
+}
+
+if (session.ready()) {
+  session.answer();
+
+  var curl = new CURL();
+
+  curl.run("POST", "http://www.freeswitch.org/curltest/", "foo=bar&this=has+spaces+in+it", my_callback, "my arg\n", "user:s3cr3tpw");
+
+  session.hangup();
+}
+
+*/
+	session = new Session('sofia/external/babcockp@iptel.org');
+    	var curl = new CURL();
+        
+	curl.run("GET", "http://babcock2-T61:8024/nextvoter/", "", logResult, "arg1", "");
+
+	session.waitForAnswer(10000);
+	 
+	if (session.ready()) {
+		session.sleep(1000);
+		session.streamFile('/usr/local/freeswitch/sounds/en/us/demos/fs-test.wav');
+	}
+
+	function logResult(string, arg) {
+		console.log("info", string);
+		return true;
+	}
